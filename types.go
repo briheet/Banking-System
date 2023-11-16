c76b8315a058363dade17b93e6a1e123ee377351
Briheet <briheetyadav@gmail.com>
2023-11-16 09:46:32 +0530

    added postgres

---
+++ types.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+	"math/rand"
+	"time"
+)
+
+type TransferRequest struct {
+	ToAccount int       `json:"toAccount"`
+	Amount    int       `json:"amount"`
+}
+
+type CreateAccountRequest struct {
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+}
+
+type Account struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func NewAccount(FirstName, LastName string) *Account {
+	return &Account {
+		FirstName : FirstName,
+		LastName :  LastName,
+		Number :    int64(rand.Intn(1000000)),
+		CreatedAt: time.Now().UTC(),
+	}
+}
